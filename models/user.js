ce136b9639b74338e921b4d0000f35eedc4557a3
Denis <den-apple@ya.ru>
2023-01-14 13:13:27 +0200

    feat: создать схему и модель user базы данных

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (v) => isEmail(v),
+        message: 'Неправильный формат почты',
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+      minlength: 8,
+    },
+    name: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 30,
+    },
+  },
+  { versionKey: false },
+);
+
+module.exports = mongoose.model('user', userSchema);
