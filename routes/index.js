80fd69b32bc8c5d79512a204e66a9f09dab90022
Denis <den-apple@ya.ru>
2023-01-15 19:29:37 +0200

    feat: создать middleware авторизации

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 const authorizationRouter = require('./authorization');
 const usersRouter = require('./users');
 const moviesRouter = require('./movies');
+const auth = require('../middlewares/auth');
 
 router.use('/', authorizationRouter);
+router.use(auth);
 router.use('/users', usersRouter);
 router.use('/movies', moviesRouter);
 
