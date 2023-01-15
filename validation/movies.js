c5938dffa0ac7a4c270b7d7d35188b3520ba2942
Denis Nikolsky <den-apple@ya.ru>
2023-01-15 22:48:11 +0200

    feat: создать валидацию celebrate роута create movie

--- validation/movies.js
+++ validation/movies.js
@@ -2,10 +2,23 @@ const { celebrate, Joi } = require('celebrate');
 
 const createMovieCelebrate = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    link: Joi.string()
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string()
       .uri({ scheme: ['http', 'https'] })
       .required(),
+    trailerLink: Joi.string()
+      .uri({ scheme: ['http', 'https'] })
+      .required(),
+    thumbnail: Joi.string()
+      .uri({ scheme: ['http', 'https'] })
+      .required(),
+    movieId: Joi.number().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
   }),
 });
 
@@ -19,15 +32,3 @@ module.exports = {
   createMovieCelebrate,
   deleteMovieCelebrate,
 };
-
-// country,
-// director,
-// duration,
-// year,
-// description,
-// image,
-// trailerLink,
-// nameRU,
-// nameEN,
-// thumbnail,
-// movieId,
