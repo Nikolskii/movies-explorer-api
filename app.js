c5dfd36ed7b30d360ee4038fdde7fbee692dee0f
Denis <den-apple@ya.ru>
2023-01-15 20:52:07 +0200

    feat: сделать валидацию celebrate авторизации (регистрации, аутентификации)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const { errors } = require('celebrate');
 const router = require('./routes');
 const errorHandler = require('./middlewares/errors');
 
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use(errors());
 app.use(errorHandler);
 
 mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
---
+++ validation/auth.js
@@ -0,0 +1,18 @@
+const { celebrate, Joi } = require('celebrate');
+
+const loginCelebrate = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+});
+
+const createUserCelebrate = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+});
+
+module.exports = { loginCelebrate, createUserCelebrate };
