f77e4429559ab991139cc21fde74d66a8a0a4f91
Denis <den-apple@ya.ru>
2023-01-16 19:23:22 +0200

    feat: реализовать логирование запросов

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const router = require('./routes');
 const errorHandler = require('./middlewares/errors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(express.json());
+app.use(requestLogger);
 app.use(router);
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
