f73a475e1ca67cd4abf0c1e4a3c130e2df02b61e
Denis <den-apple@ya.ru>
2023-01-16 19:31:02 +0200

    feat: реализовать limiter

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const helmet = require('helmet');
+const { limiter } = require('./middlewares/limiter');
 const router = require('./routes');
 const errorHandler = require('./middlewares/errors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -9,6 +11,8 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(helmet());
+app.use(limiter);
 app.use(express.json());
 app.use(requestLogger);
 app.use(router);
