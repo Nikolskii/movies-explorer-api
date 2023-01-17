cf7c8f748043a70e1c7e8981b1370cd2c4e3c2f4
Denis Nikolsky <den-apple@ya.ru>
2023-01-17 21:47:45 +0200

    fix: подключить логгер запросов до limiter

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(requestLogger);
 app.use(helmet());
 app.use(limiter);
 app.use(cors);
 app.use(express.json());
-app.use(requestLogger);
 app.use(router);
 app.use(errorLogger);
 app.use(errors());
