17fdf9f7e9a9f0a3a6d15779cf2a834fe94cb516
Denis Nikolsky <den-apple@ya.ru>
2023-01-28 13:33:21 +0200

    fix: настроить cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,13 +1,13 @@
 const cors = require('cors');
 
 module.exports = cors({
-  // origin: [
-  //   'https://nikolsky.nomoredomains.rocks',
-  //   'http://nikolsky.nomoredomains.rocks',
-  //   'http://localhost:3000',
-  // ],
-  origin: '*',
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: [
+    'https://nikolsky.nomoredomains.rocks',
+    'http://nikolsky.nomoredomains.rocks',
+    'http://localhost:3000',
+  ],
+  // origin: '*',
+  // allowedHeaders: ['Content-Type', 'Authorization'],
   optionsSuccessStatus: 200,
   credentials: true,
 });
