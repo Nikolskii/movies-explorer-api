eae4de50b0c05fc96e148fef5f8b595ad60f4bf9
Denis Nikolsky <den-apple@ya.ru>
2023-01-28 13:29:55 +0200

    chore: временно исправить конфиг cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,11 +1,13 @@
 const cors = require('cors');
 
 module.exports = cors({
-  origin: [
-    'https://nikolsky.nomoredomains.rocks',
-    'http://nikolsky.nomoredomains.rocks',
-    'http://localhost:3000',
-  ],
+  // origin: [
+  //   'https://nikolsky.nomoredomains.rocks',
+  //   'http://nikolsky.nomoredomains.rocks',
+  //   'http://localhost:3000',
+  // ],
+  origin: '*',
+  allowedHeaders: ['Content-Type', 'Authorization'],
   optionsSuccessStatus: 200,
   credentials: true,
 });
