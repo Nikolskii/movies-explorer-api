904293e5455394dff2097639f8ec9af180cb523b
Denis Nikolsky <den-apple@ya.ru>
2023-02-07 20:08:02 +0200

    chore: изменить порт

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const router = require('./routes');
 const errorHandler = require('./middlewares/errors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 const app = express();
 
 app.use(requestLogger);
