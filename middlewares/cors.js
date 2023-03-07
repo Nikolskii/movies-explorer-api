a4a29e6a51bf5a0c8f88ae52abf015c6ea64e539
Denis Nikolsky <den-apple@ya.ru>
2023-03-07 22:33:57 +0200

    fix: исправить cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -4,13 +4,9 @@ module.exports = cors({
   origin: [
     'https://nikolsky.nomoredomains.rocks',
     'http://nikolsky.nomoredomains.rocks',
+    'http://localhost:3000',
     'http://localhost:3001',
   ],
   optionsSuccessStatus: 200,
   credentials: true,
 });
-
-// module.exports = cors({
-//   origin: '*',
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-// });
